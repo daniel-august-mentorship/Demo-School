2930222a13d141acc60b84846d77feb8b1975556
august <donsw@naver.com>
2023-11-19 22:43:42 +0900

    validation2

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,21 +2,25 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import "./pages.css";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     if (username === "" || password === "") {
       setError(true);
+      setErrorMsg("Please enter username and password.");
     } else if (username === "test" && password === "test") {
-      alert("welcome");
+      setErrorMsg(false);
+      navigate("/home");
     } else {
-      setError(false);
-      alert("Invalid credentials");
+      setError(true);
+      setErrorMsg("Username and password do not match");
     }
   };
 
@@ -45,9 +49,7 @@ function Login() {
         </div>
         <br />
       </div>
-      {error && (
-        <Alert variant="warning">Username and password are required.</Alert>
-      )}
+      {error && <Alert variant="danger">{errorMsg}</Alert>}
       <Button variant="primary" size="lg" onClick={handleLogin}>
         Submit
       </Button>{" "}
